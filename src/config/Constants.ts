deca933bb55de3ca22fddeddbcf5b4b8724b5b26
Marlon <marlon.m_silva@outlook.com>
2022-07-10 23:34:12 -0400

    Ajustes

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+		'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/?retryWrites=true&w=majority',
 }
