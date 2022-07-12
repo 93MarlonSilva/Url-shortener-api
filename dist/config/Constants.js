30f9f3e95e3f08a182a27649797301c7b8481d29
Marlon Silva <marlon.m_silva@outlook.com>
2022-07-11 22:34:20 -0400

    Update Constants.js

--- dist/config/Constants.js
+++ dist/config/Constants.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
-    API_URL: 'https://urlshortner-api.herokuapp.com:8080',
+    API_URL: 'http://localhost:5000',
     MONGO_CONNECTION: 'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/?retryWrites=true&w=majority',
 };
 //# sourceMappingURL=Constants.js.map
