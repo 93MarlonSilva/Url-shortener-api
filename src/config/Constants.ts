6f156847fedc7e3391d838f447c058ef393919ae
Marlon <marlon.m_silva@outlook.com>
2022-07-13 21:40:43 -0400

    add

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,5 @@
 export const config = {
+	API_URL: 'https://urlshortner-api.herokuapp.com/',
 	MONGO_CONNECTION:
-		'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/?retryWrites=true&w=majority'
+		'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/?retryWrites=true&w=majority',
 }
