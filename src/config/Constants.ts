958f595968c48dbca16e55199e352f1de616b714
Marlon <marlon.m_silva@outlook.com>
2022-07-13 21:32:49 -0400

    Add

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,4 @@
 export const config = {
-	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/?retryWrites=true&w=majority',
+		'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/?retryWrites=true&w=majority'
 }
