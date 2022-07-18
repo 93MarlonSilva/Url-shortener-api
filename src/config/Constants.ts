3602ca4c5188a30dffb9742640d98e529c0497e8
Marlon <marlon.m_silva@outlook.com>
2022-07-17 23:35:39 -0400

    Cors

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
-	API_URL: 'https://urlshortner-api.herokuapp.com/',
+	API_URL: 'http://localhost:5500/',
 	MONGO_CONNECTION:
 		'mongodb+srv://marlon:root@url-shortener-dio.uvkat.mongodb.net/?retryWrites=true&w=majority',
 }
