01e966f48351f162e4fd88f2aab57cc7fae560ad
Marlon <marlon.m_silva@outlook.com>
2022-07-17 22:54:35 -0400

    Cors

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import { URLController } from './controller/URLController'
 import { MongoConnection } from './database/MongoConnection'
-import { config } from './config/Constants'
 
 const api = express()
 api.use(express.json())
-
-let cors = require("cors");
-api.use(cors({
-  origin: config.API_URL,
-}))
 
 const database = new MongoConnection()
 database.connect()
