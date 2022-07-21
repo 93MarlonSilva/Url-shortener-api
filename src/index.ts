d7d85ba2358c8c36a0306c5ed90aac917243e9e7
Marlon <marlon.m_silva@outlook.com>
2022-07-20 21:04:08 -0400

    teste

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,8 @@ import express from 'express'
 import { URLController } from './controller/URLController'
 import { MongoConnection } from './database/MongoConnection'
 
-const cors = require('cors')
 const api = express()
-const bodyParser = require('body-parser')
-
 api.use(express.json())
-api.use(bodyParser.urlencoded({ extended: false }))
-
-api.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", 'GET, PUT, POST, DELETE');
-    api.use(cors());
-    next();
-})
-
 
 const database = new MongoConnection()
 database.connect()
@@ -24,4 +12,6 @@ const urlController = new URLController()
 api.post('/shorten', urlController.shorten)
 api.get('/:hash', urlController.redirect)
 
-api.listen(5000, () => console.log('Express listening'))
+api.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, api.settings.env);
+});
